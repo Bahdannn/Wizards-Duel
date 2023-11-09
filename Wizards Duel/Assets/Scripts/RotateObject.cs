e0dc3e055314c908fc2724976505402d0a49371b
Bahdannn <diachuk.b@gmail.com>
2023-11-10 00:48:23 +0300

    Физический материал шаров и объекты арены

---
+++ Wizards Duel/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float RotateSpeed;
+    public void Update()
+    {
+        float angle = transform.eulerAngles.z;
+        transform.Rotate(0, 0, RotateSpeed * 1f * Time.deltaTime);
+    }
+}
